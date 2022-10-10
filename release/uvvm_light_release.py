7b4da140ee470dad0e0b82700553ce187d31e11b
UVVM <info@bitvis.no>
2022-10-10 20:52:57 +0200

    "Updated to UVVM v2022.05.05"

--- release/uvvm_light_release.py
+++ release/uvvm_light_release.py
@@ -1,10 +1,15 @@
-import subprocess, os, glob, sys, platform
+import subprocess
+import os
+import glob
+import sys
+import platform
 from shutil import copyfile
 from pprint import pprint
 from datetime import date
-# from builtins import 
+# from builtins import
 
 uvvm_github = "git@github.com:UVVM/UVVM.git"
+
 
 def prepare_internal_repo(current_dir):
     '''
@@ -13,32 +18,38 @@ def prepare_internal_repo(current_dir):
     if os.path.isdir("uvvm"):
         print(" - Updating UVVM repository")
         os.chdir("uvvm")
-        process = subprocess.Popen(["git", "pull", uvvm_github], stdout=subprocess.PIPE)
+        process = subprocess.Popen(
+            ["git", "pull", uvvm_github], stdout=subprocess.PIPE)
         process.wait()
     else:
         print(" - Cloning UVVM from GitHub")
-        process = subprocess.Popen(["git", "clone", uvvm_github], stdout=subprocess.PIPE)
+        process = subprocess.Popen(
+            ["git", "clone", uvvm_github], stdout=subprocess.PIPE)
         process.wait()
         os.chdir("uvvm")
 
     print(" - Ensure UVVM is on master branch.")
-    process = subprocess.Popen(["git", "checkout", "master"], stdout=subprocess.PIPE)
+    process = subprocess.Popen(
+        ["git", "checkout", "master"], stdout=subprocess.PIPE)
     process.wait()
     os.chdir(current_dir)
 
 
 def execute(cmd, allow_fail=False):
-    popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+    popen = subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, universal_newlines=True)
     for stdout_line in iter(popen.stdout.readline, ""):
-        yield stdout_line 
+        yield stdout_line
     popen.stdout.close()
     return_code = popen.wait()
     if return_code and allow_fail is False:
         raise subprocess.CalledProcessError(return_code, cmd)
 
+
 def execute_and_print(cmd, allow_fail=False):
     for path in execute(cmd, allow_fail):
-        print(path, end="")    
+        print(path, end="")
+
 
 def cleanup():
     '''
@@ -59,23 +70,26 @@ def copy_files(files_to_copy, current_dir):
     for item in files_to_copy:
         filename = os.path.basename(item)
         filename_with_path = os.path.abspath(item)
-        
+
         if '.vhd' in filename.lower():
             if 'uvvm_util' not in filename_with_path.lower():
-            # if 'bfm' in filename.lower() and 'bfm_common_pkg' not in filename.lower():
-                target_name = os.path.join(current_dir, '../src_bfm/', filename)
+                # if 'bfm' in filename.lower() and 'bfm_common_pkg' not in filename.lower():
+                target_name = os.path.join(
+                    current_dir, '../src_bfm/', filename)
                 print('[COPY SRC_BFM] %s --> %s' % (item, target_name))
             else:
-                target_name = os.path.join(current_dir, '../src_util/', filename)
+                target_name = os.path.join(
+                    current_dir, '../src_util/', filename)
                 print('[COPY SRC_UTIL] %s --> %s' % (item, target_name))
         else:
-            
+
             if 'build' in filename_with_path.lower() or 'source' in filename_with_path.lower():
-                
+
                 filename_with_path = filename_with_path.replace('\\', '/')
                 filename_with_path = filename_with_path.replace('uvvm/', '../')
 
-                target_name = os.path.join(current_dir, '../doc/', filename_with_path)
+                target_name = os.path.join(
+                    current_dir, '../doc/', filename_with_path)
                 print('[COPY DOC RST] %s --> %s' % (item, target_name))
 
                 path = os.path.dirname(target_name)
@@ -90,23 +104,29 @@ def copy_files(files_to_copy, current_dir):
 
 def locate_files_to_copy():
     print('Locating UVVM Util src files.')
-    files_to_copy = [filename for filename in glob.glob("./uvvm/uvvm_util/src/*.vhd")]
+    files_to_copy = [filename for filename in glob.glob(
+        "./uvvm/uvvm_util/src/*.vhd")]
     print('Locating UVVM Util doc files.')
-    files_to_copy += [filename for filename in glob.glob("./uvvm/uvvm_util/doc/*.pdf")]
-    files_to_copy += [filename for filename in glob.glob("./uvvm/uvvm_util/doc/*.pps")]
-    
+    files_to_copy += [filename for filename in glob.glob(
+        "./uvvm/uvvm_util/doc/*.pdf")]
+    files_to_copy += [filename for filename in glob.glob(
+        "./uvvm/uvvm_util/doc/*.pps")]
+
     print('Locating BFM src files.')
-    files_to_copy += [filename for filename in glob.glob("./uvvm/bitvis_*/doc/*bfm*.pdf")]
-    files_to_copy += [filename for filename in glob.glob("./uvvm/bitvis_*/src/*bfm*.vhd")]
-    
+    files_to_copy += [filename for filename in glob.glob(
+        "./uvvm/bitvis_*/doc/*bfm*.pdf")]
+    files_to_copy += [filename for filename in glob.glob(
+        "./uvvm/bitvis_*/src/*bfm*.vhd")]
+
     print('Locating RST/HTML files.')
-    files_to_copy += [filename for filename in glob.glob("./uvvm/doc/**/*.*", recursive=True)]
+    files_to_copy += [filename for filename in glob.glob(
+        "./uvvm/doc/**/*.*", recursive=True)]
 
     return files_to_copy
 
 
 def present_files(files_to_copy):
-    print(" - Files found: %i." %(len(files_to_copy)))
+    print(" - Files found: %i." % (len(files_to_copy)))
     for idx, item in enumerate(files_to_copy):
         print("(%i) --> %s" % (idx, item))
 
@@ -115,12 +135,14 @@ def test_compilation(current_dir):
     print(" - Compiling uvvm_util files.")
     execute_and_print(["vlib", "uvvm_util"])
     execute_and_print(["vmap", "work", "uvvm_util"])
-    execute_and_print(["vsim", "-c", "-do", "../script/compile.do", "-do", "exit"])
+    execute_and_print(
+        ["vsim", "-c", "-do", "../script/compile.do", "-do", "exit"])
 
     ok = input(" - Compilation ok [Y/N] ?")
     if ok.lower() == 'y':
         print(" - Compiling and running demo tb.")
-        execute_and_print(["vsim", "-c", "-do", "../sim/compile_and_run_demo_tb.do", "-do", "exit"])
+        execute_and_print(
+            ["vsim", "-c", "-do", "../sim/compile_and_run_demo_tb.do", "-do", "exit"])
         os.chdir(current_dir)
     else:
         print(" - aborting")
@@ -131,7 +153,7 @@ def test_compilation(current_dir):
         print(" - aborting")
         sys.exit(1)
 
-        
+
 def publish_github():
     print("""\n
     Preparing for publishing of changes to UVVM_Light:
@@ -144,7 +166,7 @@ def publish_github():
     """)
 
     date_tag = date.today().strftime("%y.%m.%d")
-    commit_msg = '"Updated to UVVM v2022.05.05"' # + date_tag + '"'
+    commit_msg = '"Updated to UVVM v2022.05.05"'  # + date_tag + '"'
 
     print("Setting up remote GitHub UVVM Light")
     uvvm_light_remote = "git remote add uvvm_light_remote git@github.com:UVVM/UVVM_Light.git"
@@ -158,7 +180,7 @@ def publish_github():
     execute_and_print(["git", "config", "user.email", "info@bitvis.no"])
 
     print("Deleting cloned repository uvvm")
-    os.system("rm -r uvvm")
+    os.system("rm -rf uvvm")
 
     print("Adding files and commiting")
     execute_and_print(["git", "add", "../src_bfm/."])
@@ -167,12 +189,13 @@ def publish_github():
     execute_and_print(["git", "add", "../script/."])
     execute_and_print(["git", "add", "../release/."])
     execute_and_print(["git", "add", "../demo_tb/."])
-    
+
     execute_and_print(["git", "add", "-u"])
     execute_and_print(["git", "clean", "-fdx"])
     execute_and_print(["git", "commit", "-m", commit_msg], allow_fail=True)
 
-    execute_and_print(["git", "branch", "-M", "main"])
+    #execute_and_print(["git", "branch", "-M", "main"])
+    #execute_and_print(["git", "checkout", "main"])
 
     print("On UVVM Light master branch: pushing to GitHub UVVM_Light repositoty, master branch")
     execute_and_print(["git", "push", "uvvm_light_remote"])
@@ -181,7 +204,7 @@ def publish_github():
 
 def main():
     current_dir = os.getcwd()
-    
+
     print("\nNote! This program will clone UVVM repository and update all src files on UVVM_Light repository.")
     print("If a mismatch in file numbers occure, the script will end.")
     print("After copy, the script will compile all BFM src files and run the demo TB.")
@@ -193,7 +216,7 @@ def main():
     print("%s\nCreating list of all util vhd files available:" % (80*'='))
 
     files_to_copy = locate_files_to_copy()
-    present_files(files_to_copy)   
+    present_files(files_to_copy)
     copy_files(files_to_copy, current_dir)
 
     print("\nTesting compilation and demo TB.")
@@ -205,10 +228,8 @@ def main():
     print("\nPublishing release changes to GitHub")
     publish_github()
 
-
     print("==========================================================================")
     print(" UVVM_Light RELEASE DONE\n")
-
 
 
 if __name__ == "__main__":
